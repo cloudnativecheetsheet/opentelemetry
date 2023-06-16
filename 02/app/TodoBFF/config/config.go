b7b0c966a7f8de26b89fcbad008b8fb2ad84e8ee
keisukesakasai <k6s4i5.3rx@gmail.com>
2023-06-16 23:52:25 +0900

    :recycle: Refacor code

--- 02/app/TodoBFF/config/config.go
+++ 02/app/TodoBFF/config/config.go
@@ -8,13 +8,11 @@ import (
 )
 
 type ConfigList struct {
-	Port         string
-	LogFile      string
-	Static       string
-	Deploy       string
-	EpUserApi    string
-	EpTodoApi    string
-	TraceBackend string
+	Port      string
+	LogFile   string
+	Static    string
+	EpUserApi string
+	EpTodoApi string
 }
 
 var Config ConfigList
@@ -30,12 +28,10 @@ func LoadConfig() {
 		log.Fatalln(err)
 	}
 	Config = ConfigList{
-		Port:         cfg.Section("web").Key("port").MustString("8080"),
-		LogFile:      cfg.Section("web").Key("logfile").String(),
-		Static:       cfg.Section("web").Key("static").String(),
-		Deploy:       cfg.Section("deploy").Key("env").String(),
-		EpUserApi:    cfg.Section("api").Key("ep_user_api").String(),
-		EpTodoApi:    cfg.Section("api").Key("ep_todo_api").String(),
-		TraceBackend: cfg.Section("otel").Key("trace_backend").String(),
+		Port:      cfg.Section("web").Key("port").MustString("8080"),
+		LogFile:   cfg.Section("web").Key("logfile").String(),
+		Static:    cfg.Section("web").Key("static").String(),
+		EpUserApi: cfg.Section("api").Key("ep_user_api").String(),
+		EpTodoApi: cfg.Section("api").Key("ep_todo_api").String(),
 	}
 }
